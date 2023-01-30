71b3fb0907b2b0bd9bee9c7c11af762ebeaa6239
Nada Abdelmaboud <nada.abdelmaboud@tactful.ai>
2023-01-30 19:21:17 +0200

    add recent doc

--- flask_tactful/rest/doc.py
+++ flask_tactful/rest/doc.py
@@ -3,33 +3,28 @@
 from .parser_doc import parser_doc
 
 def desc(txt=""):
-    """ highlight description """
-    return f"<h3>{txt}</h3>"
-
-def header(txt,cases=None):  
-    """ highlight header """
-    return f"<h1>{txt}</h1><br> + {usecases(cases)}"
-
+    return "<h3>{0}</h3>".format(txt)
+def header(txt,cases=None):
+    return "<h1>{0}</h1><br>".format(txt)+usecases(cases)
 def usecases(cases):
-    """ highlight usecase """
     return "<br>".join([desc(usecase) for usecase in cases]) if cases else ""
-
 def namespace_doc(description,cases=None):
-    """ highlight namespace description """
-    return f"<h2>{description}</h2><br> {usecases(cases)}"
+    return "<h2>{0}</h2><br>".format(description)+usecases(cases)
     
 def responses_doc(exceptions,custom_exceptions=None):
-    """ highlight response documentation """
     res = {}
     for exception in exceptions:
-        res[exception.code] = desc(exception.description)
+        if not res.get(exception.code):
+            res[exception.code]=""
+        res[exception.code] += desc(exception.description)
     if custom_exceptions:
         for exception in custom_exceptions:
-            res[exception] = desc(custom_exceptions[exception])
+            if not res.get(exception):
+                res[exception]=""
+            res[exception] += desc(custom_exceptions[exception])
     return res
 
 def path_ids_doc(path_params):
-    """ highlight path paremeters """
     res = {}
     for param in path_params:
         res[param]={'name': param, 'in': 'path', 'type': 'integer', 'required': True, 'description': 'The '+param+' identifier'}
