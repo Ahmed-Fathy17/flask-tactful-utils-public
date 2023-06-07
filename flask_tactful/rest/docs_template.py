49aac7015fc08f841b5500a178010aca4639c866
Mahmoud Badr <mahmoud.badr@destiny.eu>
2023-06-08 00:33:18 +0300

    replace jsdliver with unpkg

--- flask_tactful/rest/docs_template.py
+++ flask_tactful/rest/docs_template.py
@@ -20,7 +20,7 @@ template = """
   </head>
   <body>
     <redoc spec-url='{{swagger_url}}'></redoc>
-    <script src="https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js"> </script>
+    <script src="https://unpkg.com/redoc@next/bundles/redoc.standalone.js"> </script>
   </body>
 </html>
 """
