0266a67bece924e62e32ebddc766f067b8dc63b2
Omar Hafez <omar.hafez@tactful.ai>
2023-12-26 19:08:10 +0200

    Do not fail if there is no json body

--- flask_tactful/rest/jwt_decorators.py
+++ flask_tactful/rest/jwt_decorators.py
@@ -83,7 +83,7 @@ def resource_permission(resource: str, kwargs: Dict) -> bool:
             "token": token.split()[-1],
             "query_params": {resource: request.args.to_dict()},
             "path_params": {resource: kwargs},
-            "body_params": {resource: request.get_json(silent=request.method == 'GET')}
+            "body_params": {resource: request.get_json(silent=True)}
         }
     }
     if current_app.config.get("TESTING", False):
