83da1a8c7ef4f866ed52622437f094d7581847ed
Mostafa-wael <mostafa.w.k000@gmail.com>
2024-02-07 18:31:11 +0200

    ensure backward compatibility in ERP's contract

--- flask_tactful/rest/pagination.py
+++ flask_tactful/rest/pagination.py
@@ -8,7 +8,12 @@ default_general_namespace = Namespace('default')
 pagination_model = default_general_namespace.model("pagination", {
     'page': fields.Integer,
     'per_page': fields.Integer,
-    'total': fields.Integer
+    'total': fields.Integer,
+    # add for sake of backward compatibility in ERP's contract
+    # TODO: remove in future
+    "pagesCount": fields.Integer, # total number of pages
+    "limit": fields.Integer, # equivalent to per_page
+    "itemsCount": fields.Integer # equivalent to total
 })
 
 rule_model = default_general_namespace.model("Rule", {
@@ -23,7 +28,7 @@ rule_model = default_general_namespace.model("Rule", {
 pagination_parser = reqparse.RequestParser()
 pagination_parser.add_argument('page', type=int, default=1, location='args', help='Page number')
 pagination_parser.add_argument('per_page', type=int, default=1, location='args', help='Number of items per page')
-
+pagination_parser.add_argument('limit', type=int, default=10000, location='args', help='[Deprecated]Number of items per page')
 
 def envelop_pagination(namespace: Namespace, model: Model):
     """ adds pagination to returned Rest Model by adding [items] array """
