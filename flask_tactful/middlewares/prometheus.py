b0e2df323495a8895954988d263ee6441b5e9cd2
Omar Hafez <omar.hafez@tactful.ai>
2024-02-11 20:33:48 +0200

    Fix Compilation

--- flask_tactful/middlewares/prometheus.py
+++ flask_tactful/middlewares/prometheus.py
@@ -1,12 +1,12 @@
 import os
 
 from flask import Flask
-from flask_tactful.rest import RestApi
+from flask_restx import Api
 
 from prometheus_flask_exporter import RESTfulPrometheusMetrics
 
 
-def init_metrics(app: Flask, api: RestApi) -> None:
+def init_metrics(app: Flask, api: Api) -> None:
     if os.environ.get('FLASK_PROMETHEUS_ENABLED', 'false').lower() == 'true':
         app.logger.info("prometheus init")
         RESTfulPrometheusMetrics(app, api, group_by='url_rule')
