260629a0c3b25d6ff71255f39d78820035d00d00
Omar Hafez <omar.hafez@tactful.ai>
2024-02-11 22:38:37 +0200

    Added App URL Prefix to Prometheus

--- flask_tactful/middlewares/prometheus.py
+++ flask_tactful/middlewares/prometheus.py
@@ -8,5 +8,6 @@ from prometheus_flask_exporter import RESTfulPrometheusMetrics
 
 def init_metrics(app: Flask, api: Api) -> None:
     if os.environ.get('FLASK_PROMETHEUS_ENABLED', 'false').lower() == 'true':
-        app.logger.info("prometheus init")
-        RESTfulPrometheusMetrics(app, api, group_by='url_rule')
+        path = f'{api.blueprint.url_prefix}/metrics'
+        app.logger.info(f"prometheus init at {path}" )
+        RESTfulPrometheusMetrics(app, api, path=path, group_by='url_rule')
